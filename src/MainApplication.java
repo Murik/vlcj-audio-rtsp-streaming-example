30dcaf3044a4adda807bccd5ea3c9435f76914f1
Murik <MTemirov@lanit.ru>
2020-08-06 20:52:44 +0300

    fix: move to rtsp. add repeat

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -13,9 +13,10 @@ public class MainApplication {
 	    	try {
 	    		
 	    		System.out.println("Begin Streaming APP");
-				StreamRTP rtp = new StreamRTP();
+				StreamRTP rtp = new StreamRTP("127.0.0.1", 5000);
 				//rtp.start("YOUR_LOCAL_IP_HERE", PORT, "sample.mp3");
-				rtp.start("10.20.11.142", 5000, "sample.mp3");
+				rtp.start("127.0.0.1", 5000, "AAAtVintAAA!.mp3");
+
 	    		System.out.println("End Streaming APP");
 	    		
 			} catch (Exception e) {
--- src/StreamRTP.java
+++ src/StreamRTP.java
@@ -18,11 +18,12 @@ public class StreamRTP {
     private MediaListPlayer mediaListPlayer;
     private MediaList playList;
     
-    public StreamRTP() throws Exception {
+    public StreamRTP(String address, int port) throws Exception {
         boolean found = new NativeDiscovery().discover();
         if (found) {
         	System.out.println(LibVlc.INSTANCE.libvlc_get_version());
-            factory = new MediaPlayerFactory();
+            String[] mediaOptions = formatRtpStream(address, port, "demo");
+            factory = new MediaPlayerFactory(mediaOptions);
             mediaListPlayer = factory.newMediaListPlayer();
             mediaListPlayer.addMediaListPlayerEventListener(new MediaListPlayerEventAdapter() {
                 @Override
@@ -49,37 +50,40 @@ public class StreamRTP {
      * 
      * @param serverAddress
      * @param serverPort
+     * @param id
      * @return  mediaOptions for rtp stream wih vlc
      */
-    private String formatRtpStream(String serverAddress, int serverPort) {
-        StringBuilder sb = new StringBuilder(60);
-        sb.append(":sout=#rtp{dst=");
-        sb.append(serverAddress);
-        sb.append(",port=");
-        sb.append(serverPort);
-        sb.append(",mux=ts}");
-        return sb.toString();
+    private String[] formatRtpStream(String serverAddress, int serverPort, String id) {
+        // https://wiki.videolan.org/VLC_command-line_help
+        return new String[]{
+            ":sout=#rtp{sdp=rtsp://" + serverAddress +":"+serverPort+"/"+id+",proto=tcp}",
+                ":no-sout-rtp-sap", // SAP announcing (default disabled)
+                ":no-sout-standard-sap", // SAP announcing (default disabled)
+                ":sout-all", //Enable streaming of all ES (default enabled) Stream all elementary streams
+                ":rtsp-caching=100",
+                ":sout-keep",
+                ":rtsp-frame-buffer-size=1000"
+        };
     }
 
     /**
      * Start streaming of music by adding music to the playList
      * 
-     * @param dir
      * @param address
      * @param port
-     * @param musique
+     * @param music
      * @throws Exception
      */
      void start(String address, int port, String music) throws Exception {
         mediaListPlayer = factory.newMediaListPlayer();
-        String mediaOptions = formatRtpStream(address, port);
-        playList.addMedia("music/" + music, mediaOptions);
+//        String[] mediaOptions = formatRtpStream(address, port, "demo");
+        playList.addMedia("music/" + music);
         // Attach the play-list to the media list player
         mediaListPlayer.setMediaList(playList);
         mediaListPlayer.setMode(MediaListPlayerMode.LOOP);
         // Finally, start the media player
         mediaListPlayer.play();
-        System.out.println("Streaming started at rtp://" + address + ":" + port);
+        System.out.println("Streaming started at rtsp://" + address + ":" + port + "/demo");
 
     }
 
